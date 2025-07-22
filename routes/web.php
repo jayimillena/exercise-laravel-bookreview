da0530b68cd6019baa108e00ca8fb98a6f07915d
Jay Millena <jay.millena07@gmail.com>
2025-07-22 10:34:26 +0800

    Controllers & Resource Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{ 
+    BookController, ReviewController 
+};
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('review', ReviewController::class);
